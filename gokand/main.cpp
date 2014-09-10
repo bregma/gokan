03f928146a58f9d716c9c39db8646fb157fa1373
Stephen M. Webb <stephen.webb@canonical.com>
2014-09-10 12:14:09 -0400

    added skeletal gokand daemon

---
+++ gokand/main.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file gokand/main.cpp
+ * @brief gokan service daemon program entry point.
+ */
+
+/*
+ * Copyright 2014 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 3, as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <iostream>
+
+
+int
+main(int argc, char* argv[])
+{
+  try
+  {
+  }
+  catch (std::exception const& ex)
+  {
+    std::cerr << "exception caught: " << ex.what() << "\n"
+              << "exiting...\n";
+    return 1;
+  }
+  catch (...)
+  {
+    std::cerr << "unknown exception caught, "
+              << "exiting...\n";
+    return 1;
+  }
+  return 0;
+}
